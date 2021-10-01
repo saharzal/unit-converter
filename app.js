da11068862138820720e786d784aff49d0e0becb
Sahar <saharz99.sz@gmail.com>
2021-10-01 20:08:29 +0330

    edit selects and get units

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-const convertBtn = document.getElementById("convertbtn");
-const toInput = document.getElementById("to");
-const fromInput = document.getElementById("from");
+const convertBtn = document.querySelector("#convertbtn");
+const toInput = document.querySelector("#to");
+const fromInput = document.querySelector("#from");
+const testText = document.querySelector("#testText");
+const testText2 = document.querySelector("#testText2");
+const fromUnit = document.querySelector("#fromUnit");
+const toUnit = document.querySelector("#toUnit");
 
-convertBtn.onclick = function(){
-    toInput.textContent = "23";
+// convertBtn.onclick = function(){
+    
+// }
+convertBtn.addEventListener('click',()=>{
+    toInput.value = "23";
+    convertToMeter();
+});
+
+function convertToMeter(){
+    console.log(fromUnit.value);
+    console.log(toUnit.value);
+    switch(fromUnit){
+        
+    }
 }
